2d835a5a440eab1e60e7882a5b16ae7d875d7cb8
aman kumar <aman.fullstack.1192@gmail.com>
2019-09-16 00:59:24 +0530

    resolved issues in Test classes and added dependencies

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
@@ -1,14 +1,24 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
-import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
-import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
+import com.upgrad.FoodOrderingApp.service.dao.*;
 import com.upgrad.FoodOrderingApp.service.entity.*;
+import com.upgrad.FoodOrderingApp.service.exception.ItemNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.*;
 
 @Service
 public class ItemService {
+
+    @Autowired
+    private ItemDao itemDao;
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @Autowired
+    private OrderItemDao orderItemDao;
 
     @Autowired
     private RestaurantDao restaurantDao;
@@ -16,7 +26,48 @@ public class ItemService {
     @Autowired
     private CategoryDao categoryDao;
 
-    // This method is used to retreive the items of specific restaurant and specific category*/
+    /* Method to fetch item based on item UUID else would throw item not found exception*/
+    public ItemEntity getItemByUUID(String uuid) throws ItemNotFoundException {
+        ItemEntity itemEntity = itemDao.getItemByUUID(uuid);
+        if (itemEntity == null) {
+            throw new ItemNotFoundException("INF-003", "No item by this id exist");
+        }
+        return itemEntity;
+    }
+
+    /* Methods to retrieve items under specific order */
+    public List<OrderItemEntity> getItemsByOrder(OrderEntity orderEntity) {
+        return orderItemDao.getItemsByOrder(orderEntity);
+    }
+
+    /*Methods to retrieve popular items based on the restaurant*/
+    public List<ItemEntity> getItemsByPopularity(RestaurantEntity restaurantEntity) {
+        List<ItemEntity> itemEntityList = new ArrayList<>();
+        for (OrderEntity orderEntity : orderDao.getOrdersByRestaurant(restaurantEntity)) {
+            for (OrderItemEntity orderItemEntity : orderItemDao.getItemsByOrder(orderEntity)) {
+                itemEntityList.add(orderItemEntity.getItem());
+            }
+        }
+
+        // count all with map
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (ItemEntity itemEntity : itemEntityList) {
+            Integer count = map.get(itemEntity.getUuid());
+            map.put(itemEntity.getUuid(), (count == null) ? 1 : count + 1);
+        }
+
+        // sort map
+        Map<String, Integer> treeMap = new TreeMap<String, Integer>(map);
+        List<ItemEntity> sortedItemEntityList = new ArrayList<ItemEntity>();
+        for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
+            sortedItemEntityList.add(itemDao.getItemByUUID(entry.getKey()));
+        }
+        Collections.reverse(sortedItemEntityList);
+
+        return sortedItemEntityList;
+    }
+
+    /*to fetch the items of specific restaurant and for specific category*/
     public List<ItemEntity> getItemsByCategoryAndRestaurant(String restaurantUUID, String categoryUUID) {
         RestaurantEntity restaurantEntity = restaurantDao.getRestaurantByUUID(restaurantUUID);
         CategoryEntity categoryEntity = categoryDao.getCategoryByUuid(categoryUUID);
